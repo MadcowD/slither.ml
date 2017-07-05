ade25a2cc5ad923b83034adf094a778ca06242ff
William Guss <carlostnb@gmail.com>
2017-07-04 23:14:05 -0700

    Width change.

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ class AgentConfig(object):
 class EnvironmentConfig(object):
   env_name = 'Breakout-v0'
 
-  screen_width  = 84
-  screen_height = 84
+  screen_width  = 512
+  screen_height = 384
   max_reward = 1.
   min_reward = -1.
 
